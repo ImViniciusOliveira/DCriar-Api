fe6501a8049c73e22c1005c066a49366271da5d4
viniciusdev <viniciusvirada@gmail.com>
2025-10-23 21:56:26 -0300

    feat: Adiciona link para canais-venda na ApiRoot

--- src/main/java/com/dcriar/api/controller/ApiRootController.java
+++ src/main/java/com/dcriar/api/controller/ApiRootController.java
@@ -1,9 +1,10 @@
 package com.dcriar.api.controller;
 
+import com.dcriar.api.controller.product.CanalVendaController;
 import com.dcriar.api.controller.product.ProdutoController;
+import com.dcriar.api.controller.production.OrdemDeProducaoController;
 import com.dcriar.api.controller.sales.VendaController;
 import com.dcriar.api.controller.stock.LoteMateriaPrimaController;
-import com.dcriar.api.controller.production.OrdemDeProducaoController;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.hateoas.Link;
@@ -33,16 +34,14 @@ public class ApiRootController {
     public RepresentationModel<?> getRoot() {
         RepresentationModel<?> rootModel = new RepresentationModel<>();
 
-        // Construímos manualmente um link templated para produtos, incluindo os parâmetros de paginação
-        // Usa o método overload sem parâmetros (existente apenas para link building) para evitar passar nulls
         String produtosBase = linkTo(methodOn(ProdutoController.class).findAll()).toUri().toString();
-        // Adiciona o link simples (compatibilidade com front antigo) e um link templated para clientes que desejam paginação
         rootModel.add(Link.of(produtosBase).withRel("produtos"));
         rootModel.add(Link.of(produtosBase + "{?page,size,sort}").withRel("produtos-paged"));
 
         rootModel.add(linkTo(methodOn(LoteMateriaPrimaController.class).findAll()).withRel("lotes-materia-prima"));
         rootModel.add(linkTo(methodOn(VendaController.class).findAll()).withRel("vendas"));
         rootModel.add(linkTo(methodOn(OrdemDeProducaoController.class).listarTodas()).withRel("ordens-de-producao"));
+        rootModel.add(linkTo(methodOn(CanalVendaController.class).findAll()).withRel("canais-venda"));
 
         return rootModel;
     }
